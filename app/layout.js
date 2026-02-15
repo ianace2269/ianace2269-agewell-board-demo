8769af882b03b74504b422cee13cbe89975bbd0a
ianace2269 <ernie.ianace@gmail.com>
2026-02-15 14:08:39 -0600

    Update and rename layout.tsx to layout.js

--- app/layout.js
+++ app/layout.js
@@ -2,7 +2,7 @@ export const metadata = {
   title: "AgeWellAlly Board Demo"
 };
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body style={{ margin: 0 }}>{children}</body>
